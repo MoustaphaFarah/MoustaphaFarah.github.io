61f34794d9f05a08cb3660b3c15e7f4d2f1505b7
MoustaphaFarah <moustapha_farah@hotmail.com>
2020-08-05 17:32:59 +0300

    Add files via upload

--- zone_pk12/assets/js/app.js
+++ zone_pk12/assets/js/app.js
@@ -203,7 +203,7 @@ var subwayLines = L.geoJson(null, {
     });
   }
 });
-$.getJSON("data/ilot_pk12.geojson", function (data) {
+$.getJSON("data/secteur_1.geojson", function (data) {
   subwayLines.addData(data);
 });
  //Create a color dictionary based off of subway route pk 12
@@ -217,7 +217,7 @@ var subwayColors = {"1":"#ff3135", "2":"#ff3135", "3":"ff3135", "4":"#009b2e",
 var route_pk12 = L.geoJson(null, {
   style: function (feature) {
       return {
-        color: "#6E6866",
+        color: "#F6FE02",
         weight: 5,
         opacity: 1
       };
@@ -252,9 +252,110 @@ var route_pk12 = L.geoJson(null, {
     });
   }
 });
-$.getJSON("data/route_pk12.geojson", function (data) {
+$.getJSON("data/secteur_2.geojson", function (data) {
   route_pk12.addData(data);
 });
+
+ //Create a color dictionary based off of subway route pk 12
+var subwayColors = {"1":"#ff3135", "2":"#ff3135", "3":"ff3135", "4":"#009b2e",
+    "5":"#009b2e", "6":"#009b2e", "7":"#ce06cb", "A":"#fd9a00", "C":"#fd9a00",
+    "E":"#fd9a00", "SI":"#fd9a00","H":"#fd9a00", "Air":"#ffff00", "B":"#ffff00",
+    "D":"#ffff00", "F":"#ffff00", "M":"#ffff00", "G":"#9ace00", "FS":"#6e6e6e",
+    "GS":"#6e6e6e", "J":"#976900", "Z":"#976900", "L":"#969696", "N":"#ffff00",
+    "Q":"#ffff00", "R":"#ffff00" };
+
+var routes_pk12 = L.geoJson(null, {
+  style: function (feature) {
+      return {
+        color: "#34FE02",
+        weight: 5,
+        opacity: 1
+      };
+  },
+  onEachFeature: function (feature, layer) {
+    if (feature.properties) {
+      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Nom</th><td>" + feature.properties.Revetement + "</td></tr>"  + "<table>";
+      layer.on({
+        click: function (e) {
+          $("#feature-title").html(feature.properties.Revetement);
+          $("#feature-info").html(content);
+          $("#featureModal").modal("show");
+
+        }
+      });
+    }
+    layer.on({
+      mouseover: function (e) {
+        var layer = e.target;
+        layer.setStyle({
+          weight: 3,
+          color: "#00FFFF",
+          opacity: 1
+        });
+        if (!L.Browser.ie && !L.Browser.opera) {
+          layer.bringToFront();
+        }
+      },
+      mouseout: function (e) {
+        routes_pk12.resetStyle(e.target);
+      }
+    });
+  }
+});
+$.getJSON("data/secteur_3.geojson", function (data) {
+  routes_pk12.addData(data);
+});
+
+ //Create a color dictionary based off of subway route pk 12
+var subwayColors = {"1":"#ff3135", "2":"#ff3135", "3":"ff3135", "4":"#009b2e",
+    "5":"#009b2e", "6":"#009b2e", "7":"#ce06cb", "A":"#fd9a00", "C":"#fd9a00",
+    "E":"#fd9a00", "SI":"#fd9a00","H":"#fd9a00", "Air":"#ffff00", "B":"#ffff00",
+    "D":"#ffff00", "F":"#ffff00", "M":"#ffff00", "G":"#9ace00", "FS":"#6e6e6e",
+    "GS":"#6e6e6e", "J":"#976900", "Z":"#976900", "L":"#969696", "N":"#ffff00",
+    "Q":"#ffff00", "R":"#ffff00" };
+
+var routees_pk12 = L.geoJson(null, {
+  style: function (feature) {
+      return {
+        color: "#02E3FE",
+        weight: 5,
+        opacity: 1
+      };
+  },
+  onEachFeature: function (feature, layer) {
+    if (feature.properties) {
+      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Nom</th><td>" + feature.properties.Revetement + "</td></tr>"  + "<table>";
+      layer.on({
+        click: function (e) {
+          $("#feature-title").html(feature.properties.Revetement);
+          $("#feature-info").html(content);
+          $("#featureModal").modal("show");
+
+        }
+      });
+    }
+    layer.on({
+      mouseover: function (e) {
+        var layer = e.target;
+        layer.setStyle({
+          weight: 3,
+          color: "#00FFFF",
+          opacity: 1
+        });
+        if (!L.Browser.ie && !L.Browser.opera) {
+          layer.bringToFront();
+        }
+      },
+      mouseout: function (e) {
+        routees_pk12.resetStyle(e.target);
+      }
+    });
+  }
+});
+$.getJSON("data/secteur_4.geojson", function (data) {
+  routees_pk12.addData(data);
+});
+
 /* Single marker cluster layer to hold all clusters */
 var markerClusters = new L.MarkerClusterGroup({
   spiderfyOnMaxZoom: true,
@@ -280,7 +381,7 @@ var poteau = L.geoJson(null, {
   },
   onEachFeature: function (feature, layer) {
     if (feature.properties) {
-      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>NOM</th><td>" + feature.properties.repondant + "</td></tr>" + "<tr><th>SEX</th><td>" + feature.properties.sexrepondant + "</td></tr>" + "<tr><th>AGE</th><td>" + feature.properties.age + "</td></tr>" + "<tr><th>Nationalté</th><td>" + feature.properties.nationalite + "</td></tr>" + "</td></tr>"  + "<tr><th>Localité</th><td>" + feature.properties.localite + "</td></tr>" + "</td></tr>"   +"<table>";
+      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>NOM</th><td>" + feature.properties.repondant + "</td></tr>" + "<tr><th>SEX</th><td>" + feature.properties.sexreponda + "</td></tr>" + "<tr><th>AGE</th><td>" + feature.properties.age + "</td></tr>" + "<tr><th>Nationalté</th><td>" + feature.properties.nationalit + "</td></tr>" + "</td></tr>"  + "<tr><th>Quartier</th><td>" + feature.properties.localite + "</td></tr>" + "</td></tr>"   +"<table>";
       layer.on({
         click: function (e) {
           $("#feature-title").html(feature.properties.NAME);
@@ -301,7 +402,7 @@ var poteau = L.geoJson(null, {
     }
   }
 });
-$.getJSON("data/sagar.geojson", function (data) {
+$.getJSON("data/enquetes.geojson", function (data) {
   poteau.addData(data);
   map.addLayer(poteauLayer);
 });
@@ -464,8 +565,11 @@ var groupedOverlays = {
   },
   "Reference": {
     "Zone d'etude": boroughs,
-     "ilot": subwayLines,
-     "Route": route_pk12
+     "Secteur 1": subwayLines,
+     "Secteur 2": route_pk12,
+     "Secteur 3": routes_pk12,
+     "Secteur 4": routees_pk12
+
   }
 };
 
