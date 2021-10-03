aaf484d349aa99049e0627f1c8068871077e1970
MoustaphaFarah <moustapha_farah@hotmail.com>
2021-10-03 11:20:16 +0300

    Add files via upload

--- zero_bidonville/assets/js/app.js
+++ zero_bidonville/assets/js/app.js
@@ -934,7 +934,7 @@ var attributionControl = L.control({
 });
 attributionControl.onAdd = function (map) {
   var div = L.DomUtil.create("div", "leaflet-control-attribution");
-  div.innerHTML = "<span class='hidden-xs'>Développé par l'EQUIPE du SIG PRESIDENCE  | </span>";
+  div.innerHTML = "<span class='hidden-xs'> DJIB-CARTO  | </span>";
   return div;
 };
 map.addControl(attributionControl);
